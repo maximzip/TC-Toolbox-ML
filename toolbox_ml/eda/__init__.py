c28dfb64f184280092c41567fd205cd04d66c545
maximzip <maksymchaikamc@gmail.com>
2026-05-29 18:39:12 +0200

    feat: creación de archivos y función describe

---
+++ toolbox_ml/eda/__init__.py
@@ -0,0 +1,8 @@
+from .core import (
+    describe_df,
+    tipifica_variables,
+    get_features_num_regression,
+    plot_features_num_regression,
+    get_features_cat_regression,
+    plot_features_cat_regression,
+)
